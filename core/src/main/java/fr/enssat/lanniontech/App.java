d5ab269a93592da4ee40cd0066b534737e982412
hiveship <maelig.nantel@gmail.com>
2016-09-20 11:40:35 +0200

    Add logging framework

--- core/src/main/java/fr/enssat/lanniontech/App.java
+++ core/src/main/java/fr/enssat/lanniontech/App.java
@@ -1,11 +1,17 @@
 package fr.enssat.lanniontech;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 /**
  * Created by maelig on 19/09/2016.
  */
 public class App {
 
+    private static Logger LOG = LoggerFactory.getLogger(App.class);
+
     public static int foo() {
+        LOG.debug("foo() returns 4");
         return 4;
     }
 }
