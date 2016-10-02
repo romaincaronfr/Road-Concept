bb1a6fa3eaf420448e78e8ab82b613b94ceada14
Webrom <webrom37@gmail.com>
2016-10-02 14:42:54 +0200

    Add some JS to test, not to be use yet
    #ROADC-73

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -1,12 +1,24 @@
 /**
  * Created by Romain on 28/09/2016.
  */
-
+var apiURL = "localhost:8080";
+Backbone.Collection.prototype.absURL = "http://localhost:8080";
 var loginToken = null;
 
-if (!loginToken){
+var setHeader = function (xhr) {
+    console.log('call setHeader');
+    xhr.setRequestHeader('Cookie', 'vertx-web.session=' + loginToken);
+}
+
+$.ajaxSetup({
+    xhrFields: {
+        withCredentials: true
+    },
+    crossDomain: true
+});
+if (!loginToken) {
     $.ajax({
-            url: "http://localhost:8080/login",
+            url: "http://" + apiURL + "/login",
             type: "POST",
             headers: {
                 "Content-Type": "application/json; charset=utf-8",
@@ -17,19 +29,22 @@ if (!loginToken){
                 "password": "fgreger"
             })
         })
-        .done(function(data, textStatus, jqXHR) {
+        .done(function (data, textStatus, jqXHR) {
             console.log("HTTP Request Succeeded: " + jqXHR.status);
             console.log(data);
+            loginToken = data.authenticationToken;
+            //document.cookie = "vertx-web.session="+loginToken;
+            console.log(loginToken);
+            testTech();
         })
-        .fail(function(jqXHR, textStatus, errorThrown) {
+        .fail(function (jqXHR, textStatus, errorThrown) {
             console.log("HTTP Request Failed");
         })
-        .always(function() {
+        .always(function () {
             /* ... */
         });
-
-
 }
+
 
 var app = {
 
@@ -37,16 +52,18 @@ var app = {
 
     models: {},
 
-    loadTemplates: function(views, callback) {
+    collections: {},
+
+    loadTemplates: function (views, callback) {
 
         var deferreds = [];
 
-        $.each(views, function(index, view) {
+        $.each(views, function (index, view) {
             if (app[view]) {
-                deferreds.push($.get('Templates/' + view + '.html', function(data) {
+                deferreds.push($.get('Templates/' + view + '.html', function (data) {
                     app[view].prototype.template = _.template(data);
                 }, 'html'));
-                console.log("loading view : "+view);
+                console.log("loading view : " + view);
             } else {
                 console.log(view + " not found");
             }
@@ -57,6 +74,18 @@ var app = {
 
 };
 
+app.models.mapModel = Backbone.Model.extend({});
+
+app.collections.mapCollection = Backbone.Collection.extend({
+    model: app.models.mapModel,
+    url: function () {
+        return this.absURL + '/api/maps';
+    }
+});
+var testModel = new app.models.mapModel();
+var testCollection = new app.collections.mapCollection();
+
+
 
 $(document).ready(function () {
     console.log("document ready");
@@ -65,4 +94,12 @@ $(document).ready(function () {
             app.router = new app.Router();
             Backbone.history.start();
         });
+    console.log(testCollection.get());
 });
+
+function testTech(){
+    testCollection.fetch();
+    console.log(testCollection.toJSON());
+    testCollection.add({"id":1,"name":"Ville de Lannion"});
+    console.log(testCollection.toJSON());
+}
