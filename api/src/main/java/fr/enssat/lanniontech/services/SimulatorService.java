b9ddd17179debbe9254ea99f1915d9b7b4d56592
hiveship <maelig.nantel@gmail.com>
2016-10-03 10:37:01 +0200

    Before parent project commit

---
+++ api/src/main/java/fr/enssat/lanniontech/services/SimulatorService.java
@@ -0,0 +1,8 @@
+package fr.enssat.lanniontech.services;
+
+public class SimulatorService {
+
+    public boolean simulate() {
+        return false;
+    }
+}
---
+++ api/src/main/java/fr/enssat/lanniontech/verticles/SimulatorVerticle.java
@@ -0,0 +1,45 @@
+package fr.enssat.lanniontech.verticles;
+
+import fr.enssat.lanniontech.entities.MapInfo;
+import fr.enssat.lanniontech.entities.User;
+import fr.enssat.lanniontech.services.SimulatorService;
+import fr.enssat.lanniontech.utilities.Constants;
+import fr.enssat.lanniontech.verticles.utilities.HttpResponseBuilder;
+import io.vertx.core.AbstractVerticle;
+import io.vertx.core.http.HttpMethod;
+import io.vertx.ext.web.Router;
+import io.vertx.ext.web.RoutingContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class SimulatorVerticle extends AbstractVerticle {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(SimulatorVerticle.class);
+
+    private SimulatorService simulatorService = new SimulatorService();
+
+    private Router router;
+
+    public SimulatorVerticle(Router router) {
+        this.router = router;
+    }
+
+    @Override
+    public void start() {
+
+        router.route(HttpMethod.GET, "/api/simulate").handler(routingContext -> {
+            processSimulation(routingContext);
+        });
+    }
+
+    private void processSimulation(RoutingContext routingContext) {
+        try {
+            User currentUser = (User) routingContext.session().get(Constants.SESSION_CURRENT_USER);
+            boolean result = simulatorService.simulate(); // TODO: Add parameters
+            HttpResponseBuilder.buildOkResponse(routingContext, result);
+        } catch (Exception e) {
+            HttpResponseBuilder.buildUnexpectedErrorResponse(routingContext, e);
+        }
+    }
+
+}
