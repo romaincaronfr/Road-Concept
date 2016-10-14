70a300cab84b6c280fb9e0b290b1403eb7013dd7
hiveship <maelig.nantel@gmail.com>
2016-10-14 15:23:20 +0200

    Script to update docker jar and deploy containers
    #ROADC-123

---
+++ developper-bin/updateDockerAndDeploy.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+cd ../Road\ Concept\ Backend/ && mvn clean install -DskipTests && cp Road\ Concept\ API/target/road-concept-api-0.1-SNAPSHOT-fat.jar ../Docker/roadconcept && cd ../Docker/roadconcept && docker-compose  build && docker-compose up ;
