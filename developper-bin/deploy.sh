b0d8068ca31554bf20c4b566ec28a530f5cc43b1
Maelig Nantel <maelig.nantel@gmail.com>
2016-10-24 09:33:39 +0200

    Delete deploy.sh

--- developper-bin/deploy.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-cd ../Road\ Concept\ Backend/;
-cd ../Docker/roadconcept;
-docker-compose -f ../hiveship-docker-compose.yml build && docker-compose -f ../hiveship-docker-compose.yml up;
-cd ../../developper-bin;
