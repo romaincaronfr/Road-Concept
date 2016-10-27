7707ebac5f9dad375e205c72952385f7fcc96e8f
4r3 <4r3@users.noreply.github.com>
2016-10-27 09:56:22 +0200

    Add unit test for EndRoadTrajectory
    #ROADC-147

---
+++ Road Concept Backend/Road Concept Core/src/test/java/trajectory/testEndRoadTrajectory.java
@@ -0,0 +1,38 @@
+package trajectory;
+
+
+import fr.enssat.lanniontech.core.positioning.Position;
+import fr.enssat.lanniontech.core.roadElements.RoadSection;
+import fr.enssat.lanniontech.core.trajectory.EndRoadTrajectory;
+import fr.enssat.lanniontech.core.trajectory.SimpleTrajectory;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class testEndRoadTrajectory {
+
+    @Test
+    public void test1EdgeClosing(){
+        Position A = new Position(0,0);
+        Position B = new Position(0,1);
+
+        RoadSection RS = new RoadSection(A,B,null);
+
+        Assert.assertNull(RS.getLaneAB().getInsertTrajectory().getNext());
+        Assert.assertNull(RS.getLaneBA().getInsertTrajectory().getNext());
+
+        SimpleTrajectory AB = RS.getLaneAB().getInsertTrajectory();
+        SimpleTrajectory BA = RS.getLaneBA().getInsertTrajectory();
+
+        EndRoadTrajectory BB = new EndRoadTrajectory(AB,BA);
+
+        Assert.assertEquals(BB,AB.getNext());
+        Assert.assertEquals(BA,BB.getNext());
+
+        Assert.assertNull(BA.getNext());
+
+        EndRoadTrajectory AA = new EndRoadTrajectory(BA,AB);
+
+        Assert.assertEquals(AA,BA.getNext());
+        Assert.assertEquals(AB,AA.getNext());
+    }
+}
