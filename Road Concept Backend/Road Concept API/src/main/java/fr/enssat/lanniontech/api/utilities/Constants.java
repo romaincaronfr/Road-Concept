86014e0e92bd53322d3b9d6568d029ab68a4d316
hiveship <maelig.nantel@gmail.com>
2016-10-31 11:39:31 +0100

    Fix constants

--- Road Concept Backend/Road Concept API/src/main/java/fr/enssat/lanniontech/api/utilities/Constants.java
+++ Road Concept Backend/Road Concept API/src/main/java/fr/enssat/lanniontech/api/utilities/Constants.java
@@ -21,9 +21,9 @@ public final class Constants {
 
     public static final String POSTGRESQL_DATABASE_NAME = "roadconcept";
     public static final String POSTGRESQL_USER_NAME = "roadconcept";
-    //public static final String POSTGRESQL_USER_PASSWORD = "roadconcept";
+    public static final String POSTGRESQL_USER_PASSWORD = "roadconcept";
     public static final String POSTGRESQL_SERVER_HOST = "postgresql";
-    public static final String POSTGRESQL_SERVER_HOST = "localhost";
+   // public static final String POSTGRESQL_SERVER_HOST = "localhost";
 
     public static final int POSTGRESQL_MAX_CONNECTIONS = 100;
 
