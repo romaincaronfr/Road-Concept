5ec91fcbee48ab2d82ba5cd8f7c9568ce319d246
4r3 <4r3@users.noreply.github.com>
2016-11-01 18:11:34 +0100

    Add prodution deploy script

---
+++ Docker/startProd.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+rm -rf ./nginx/frontend
+cp -r ../frontend/ ./nginx/frontend/
+
+sed -ri.bak 's/(http:\/\/localhost:8080)/http:\/\/roadconcept\.4r3\.fr:8081/' ./nginx/frontend/js/app.js
+rm ./nginx/frontend/js/app.js.bak
+
+#stop running containers
+docker-compose --tlsverify --tlscacert=certs/ca.pem --tlscert=certs/cert.pem --tlskey=certs/key.pem -H=roadconcept.4r3.fr:4243 -f docker-compose-prod.yml stop
+
+#build service
+docker-compose --tlsverify --tlscacert=certs/ca.pem --tlscert=certs/cert.pem --tlskey=certs/key.pem -H=roadconcept.4r3.fr:4243 -f docker-compose-prod.yml build
+
+#start service
+docker-compose --tlsverify --tlscacert=certs/ca.pem --tlscert=certs/cert.pem --tlskey=certs/key.pem -H=roadconcept.4r3.fr:4243 -f docker-compose-prod.yml up -d
+
