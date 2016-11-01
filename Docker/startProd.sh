3ed2d701a05691eae222ccab6b9ac73c0789536e
4r3 <4r3@users.noreply.github.com>
2016-11-01 18:43:49 +0100

    Add maven build before docker compose build

--- Docker/startProd.sh
+++ Docker/startProd.sh
@@ -6,6 +6,8 @@ cp -r ../frontend/ ./nginx/frontend/
 sed -ri.bak 's/(http:\/\/localhost:8080)/http:\/\/roadconcept\.4r3\.fr:8081/' ./nginx/frontend/js/app.js
 rm ./nginx/frontend/js/app.js.bak
 
+cd ../Road\ Concept\ Backend/ && mvn clean install -DskipTests && cp Road\ Concept\ API/target/road-concept-api-0.1-SNAPSHOT-fat.jar ../Docker/roadconcept && cd ../Docker
+
 #stop running containers
 docker-compose --tlsverify --tlscacert=certs/ca.pem --tlscert=certs/cert.pem --tlskey=certs/key.pem -H=roadconcept.4r3.fr:4243 -f docker-compose-prod.yml stop
 
