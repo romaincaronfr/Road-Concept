cb57980abf7900ba8f79c51bb7c972daa83b761e
4r3 <4r3@users.noreply.github.com>
2016-11-02 12:27:40 +0100

    Add local prod starter

---
+++ Docker/startProdLocal.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+rm -rf ./nginx/frontend
+cp -r ../frontend/ ./nginx/frontend/
+
+rm ./nginx/frontend/js/app.js.bak
+
+cd ../Road\ Concept\ Backend/ && mvn clean install -DskipTests && cp Road\ Concept\ API/target/road-concept-api-0.1-SNAPSHOT-fat.jar ../Docker/roadconcept && cd ../Docker
+
+#stop running containers
+docker-compose -f docker-compose-prod.yml stop
+
+#build service
+docker-compose -f docker-compose-prod.yml build
+
+#start service
+docker-compose -f docker-compose-prod.yml up -d
+
