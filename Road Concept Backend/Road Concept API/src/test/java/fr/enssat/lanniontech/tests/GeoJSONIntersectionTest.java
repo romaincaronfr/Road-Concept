d3f46aec74edcb03f8701837368e3a02f115a6e7
hiveship <maelig.nantel@gmail.com>
2016-11-03 14:45:27 +0100

    add test for intersections

---
+++ Road Concept Backend/Road Concept API/src/test/java/fr/enssat/lanniontech/tests/GeoJSONIntersectionTest.java
@@ -0,0 +1,18 @@
+package fr.enssat.lanniontech.tests;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class GeoJSONIntersectionTest {
+
+    @Test
+    public void testSuccessOSM() {
+        try {
+
+        } catch (Exception e) {
+            // Should not happen
+            e.printStackTrace();
+            Assert.fail();
+        }
+    }
+}
