c0ca5045782a2f6b682d0e2f9c973be4fc18916c
hiveship <maelig.nantel@gmail.com>
2016-11-08 09:24:43 +0100

    Restore constants

---
+++ Road Concept Backend/Road Concept API/src/main/java/fr/enssat/lanniontech/api/utilities/Constants.java
@@ -0,0 +1,43 @@
+package fr.enssat.lanniontech.api.utilities;
+
+public final class Constants {
+
+    public static final ProjetEnvironment ENVIRONMENT = ProjetEnvironment.DEVELOPPMENT;
+    public static final int HTTP_SERVER_PORT = 8080;
+    public static final String SESSION_CURRENT_USER = "me";
+
+    // =======
+    // MONGODB
+    // =======
+
+    public static final String MONGODB_DATABASE_NAME = "RoadConcept";
+    public static final String MONGODB_SERVER_URL = "mongodb";
+    //public static final String MONGODB_SERVER_URL = "localhost";
+    public static final int MONGODB_SERVER_PORT = 27017;
+
+    // ==========
+    // POSTGRESQL
+    // ==========
+
+    public static final String POSTGRESQL_DATABASE_NAME = "roadconcept";
+    public static final String POSTGRESQL_USER_NAME = "roadconcept";
+    public static final String POSTGRESQL_USER_PASSWORD = "roadconcept";
+    public static final String POSTGRESQL_SERVER_HOST = "postgresql";
+    //public static final String POSTGRESQL_SERVER_HOST = "localhost";
+
+    public static final int POSTGRESQL_MAX_CONNECTIONS = 100;
+
+    // ERROR CODES
+    // -----------
+    public static final String POSTGRESQL_FOREIGN_KEY_VIOLATION = "23503";
+    public static final String POSTGRESQL_UNIQUE_VIOLATION = "23505";
+
+    // ====================
+    // SINGLETON MANAGEMENT
+    // ====================
+
+    private Constants() {
+        // Prevent instantiation
+    }
+
+}
