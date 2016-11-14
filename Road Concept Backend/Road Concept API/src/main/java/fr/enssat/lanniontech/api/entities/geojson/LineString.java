077c552612ce904e311173a462b15f2d948fab41
4r3 <4r3@users.noreply.github.com>
2016-11-14 18:26:28 +0100

    First implement for init and clean

--- Road Concept Backend/Road Concept API/src/main/java/fr/enssat/lanniontech/api/entities/geojson/LineString.java
+++ Road Concept Backend/Road Concept API/src/main/java/fr/enssat/lanniontech/api/entities/geojson/LineString.java
@@ -9,6 +9,15 @@ public class LineString extends MultiPoint {
         super(points);
     }
 
+    public boolean contains(Coordinates point){
+        return getCoordinates().contains(point);
+    }
+
+    public boolean isFirstOrLast(Coordinates point){
+        return getCoordinates().getFirst().equals(point) ||
+                getCoordinates().getLast().equals(point);
+    }
+
     @Override
     public String toString() {
         return "LineString{} " + super.toString();
---
+++ Road Concept Backend/Road Concept API/src/main/java/fr/enssat/lanniontech/api/services/ExperimentalIntersections.java
@@ -0,0 +1,58 @@
+package fr.enssat.lanniontech.api.services;
+
+import fr.enssat.lanniontech.api.entities.geojson.Coordinates;
+import fr.enssat.lanniontech.api.entities.geojson.Feature;
+import fr.enssat.lanniontech.api.entities.geojson.FeatureCollection;
+import fr.enssat.lanniontech.api.entities.geojson.LineString;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class ExperimentalIntersections {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(ExperimentalIntersections.class);
+
+    private FeatureCollection myMap;
+    private Map<Coordinates,List<Feature>> explosionPivot;
+
+    public ExperimentalIntersections(FeatureCollection map){
+        myMap = map;
+        initExplosionPivot();
+        LOGGER.debug("explosionPivot size = " + explosionPivot);
+        cleanExplosionPivot();
+        LOGGER.debug("explosionPivot size = " + explosionPivot);
+    }
+
+    private void initExplosionPivot(){
+        explosionPivot = new HashMap<>();
+        for (Feature f :myMap.getFeatures()){
+            if(f.getGeometry() instanceof LineString){
+                LineString road = (LineString)f.getGeometry();
+                for (Coordinates c :road.getCoordinates()){
+                    explosionPivot.putIfAbsent(c,new ArrayList<>());
+                    explosionPivot.get(c).add(f);
+                }
+            }
+        }
+    }
+
+    private void cleanExplosionPivot(){
+        for (Coordinates c:explosionPivot.keySet()){
+            if(explosionPivot.get(c).size()<2){
+                explosionPivot.remove(c);
+            }else{
+                boolean remove = true;
+                for (Feature f :explosionPivot.get(c)){
+                    remove &= ((LineString)f.getGeometry()).isFirstOrLast(c);
+                }
+                if(remove){
+                    explosionPivot.remove(c);
+                }
+            }
+        }
+    }
+}
