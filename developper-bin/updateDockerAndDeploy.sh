8207f4875f916631e0309af1cd1e64333abf0e3d
hiveship <maelig.nantel@gmail.com>
2016-11-24 08:35:55 +0100

    Restore tests

--- developper-bin/updateDockerAndDeploy.sh
+++ developper-bin/updateDockerAndDeploy.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-cd ../Road\ Concept\ Backend/ && mvn clean install -DskipTests && cp Road\ Concept\ API/target/road-concept-api-0.1-SNAPSHOT-fat.jar ../Docker/roadconcept && cd ../Docker/roadconcept && docker-compose  build && docker-compose up ;
+cp -R ../frontend ../Docker/nginx && cd ../Road\ Concept\ Backend/ && mvn clean install && cp Road\ Concept\ API/target/road-concept-api-0.1-SNAPSHOT-fat.jar ../Docker/roadconcept && cd ../Docker/roadconcept && docker-compose  build && docker-compose up ;
