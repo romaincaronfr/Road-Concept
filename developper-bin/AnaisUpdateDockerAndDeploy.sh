d80b991e863236ca78f10383f9efb2e7323d7ce6
Anais Galisson <anais.galisson@gmail.com>
2016-11-26 17:31:53 +0100

    Update

--- developper-bin/AnaisUpdateDockerAndDeploy.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-cp -R ../frontend ../Docker/nginx && cd ../Road\ Concept\ Backend/ && mvn clean install && cp Road\ Concept\ API/target/road-concept-api-0.1-SNAPSHOT-fat.jar ../Docker/roadconcept && cd ../Docker/roadconcept && docker-compose  build && docker-compose up ;
