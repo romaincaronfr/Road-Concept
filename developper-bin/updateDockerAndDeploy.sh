01d71a03a4a801a98518b52adb4c03491e5801d8
hiveship <maelig.nantel@gmail.com>
2016-11-26 14:48:32 +0100

    Changement de nom du fichier docker-compose (pour lancement sur poste Anaïs)

--- developper-bin/updateDockerAndDeploy.sh
+++ developper-bin/updateDockerAndDeploy.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-cp -R ../frontend ../Docker/nginx && cd ../Road\ Concept\ Backend/ && mvn clean install && cp Road\ Concept\ API/target/road-concept-api-0.1-SNAPSHOT-fat.jar ../Docker/roadconcept && cd ../Docker/roadconcept && docker-compose  build && docker-compose up ;
+cp -R ../frontend ../Docker/nginx && cd ../Road\ Concept\ Backend/ && mvn clean install && cp Road\ Concept\ API/target/road-concept-api-0.1-SNAPSHOT-fat.jar ../Docker/roadconcept && cd ../Docker/roadconcept && docker-compose -f ../docker-compose-dev.yml build && docker-compose -f ../docker-compose-dev.yml up;
+
