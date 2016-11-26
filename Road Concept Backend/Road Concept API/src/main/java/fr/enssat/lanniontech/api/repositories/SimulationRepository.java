587b516695a9d659341851d668485c073656a769
hiveship <maelig.nantel@gmail.com>
2016-11-26 14:16:40 +0100

    Ajout du squelette de fonctions manquantes

--- Road Concept Backend/Road Concept API/src/main/java/fr/enssat/lanniontech/api/repositories/SimulationRepository.java
+++ Road Concept Backend/Road Concept API/src/main/java/fr/enssat/lanniontech/api/repositories/SimulationRepository.java
@@ -1,13 +1,20 @@
 package fr.enssat.lanniontech.api.repositories;
 
+import fr.enssat.lanniontech.api.entities.simulation.Simulation;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.util.UUID;
 
 public abstract class SimulationRepository extends AbstractRepository {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(SimulationRepository.class);
 
-    // TODO: Stockage SQL (définir schéma SQL)
-    // TODO: Dupliquer la map associé lors de la création d'une simulation. Comment gérer l'ID de la map ?
-    // TODO: Voir FFNEXParser pour le cloneur
+    public void duplicateFeatures(Simulation simulation) {
+        //TODO: Utiliser mongodb.cloneCollection ou mongodb.copyTo
+    }
+
+    public void deleteAssociatedFeatures(UUID simulationUUID) {
+        //TODO
+    }
 }
--- Road Concept Backend/Road Concept API/src/main/java/fr/enssat/lanniontech/api/repositories/SimulationResultRepository.java
+++ Road Concept Backend/Road Concept API/src/main/java/fr/enssat/lanniontech/api/repositories/SimulationResultRepository.java
@@ -3,6 +3,8 @@ package fr.enssat.lanniontech.api.repositories;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.UUID;
+
 public class SimulationResultRepository extends SimulationRepository {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(SimulationResultRepository.class);
@@ -11,8 +13,24 @@ public class SimulationResultRepository extends SimulationRepository {
     // CONGESTION
     // ==========
 
+    public void addRoadMetric(UUID simulationUUID, UUID roadId, int congestion) {
+        //TODO
+    }
+
     // ========
     // VEHICLES
     // ========
+
+    public void addVehicleInfo(UUID simulationUUID, int id, long time, double angle) {
+        //TODO
+    }
+
+    // ======
+    // COMMON
+    // ======
+
+    public void delete(UUID simulationUUID){
+        // TODO
+    }
 
 }
