79c33fc778e0fc861e9205a2a5761ec801ead119
Andreas <andreas.ducluzeau@gmail.com>
2016-12-07 11:28:05 +0100

    update deploy script

--- developper-bin/updateDockerAndDeploy.sh
+++ developper-bin/updateDockerAndDeploy.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-cp -R ../frontend ../Docker/nginx && cd ../Road\ Concept\ Backend/ && mvn clean install && cp Road\ Concept\ API/target/road-concept-api-0.1-SNAPSHOT-fat.jar ../Docker/roadconcept && cd ../Docker/roadconcept && docker-compose -f ../docker-compose-dev.yml build && docker-compose -f ../docker-compose-dev.yml up;
+cp -R ../frontend ../Docker/nginx && cd ../Road\ Concept\ Backend/ && mvn clean install && cp Road\ Concept\ API/target/road-concept-api-0.6-SNAPSHOT-fat.jar ../Docker/roadconcept && cd ../Docker/roadconcept && docker-compose -f ../docker-compose-dev.yml build && docker-compose -f ../docker-compose-dev.yml up;
 
